af6bfc77ea9996c1a5f3ccc9acb463032e2b609f
udeodhar <deodhar.unmesh@gmail.com>
2017-04-19 16:03:56 -0700

    Update Perceptron.py

--- Perceptron.py
+++ Perceptron.py
@@ -2,208 +2,166 @@ import numpy
 import  random
 import time
 import numpy as np
-from joblib import Parallel, delayed
-import multiprocessing
+import matplotlib.pyplot as plt
+import pandas
 
 """
 Global variables
 """
-exceptedOutput = 10
-num_cores = multiprocessing.cpu_count()
-confusionMatrix = []
 learningRate = 0.001
-inputData = []
-weights = []
 perceptron = []
-"""
-Reading CSV file
-"""
-import csv
-with open('mnist_train.csv', 'r') as mnist_trainCSV:
-	data = csv.reader(mnist_trainCSV)
-	index = 0
-	print "Reading data from CSV file..."
-	for row in data:
-		inputData.append(row)
-	
+testAccuracy = []
+trainingAccuracy = []
+
 #####################Perceptron Class#####################
 class Perceptron:
 	
 	def __init__(self):
-		self.weights = []
-		self.inputVector = [1]
-		self.expectedOutput = 10
-
+		self.weights = np.random.uniform(-0.5,0.5,785)	#Assigning ran] weights
+		self.inputVector = np.zeros(785)
+		self.inputVector[0] = 1.0	#Initializing input vector with bias
+	
 	def setWeights(self, vector):
-		self.weights = vector	
+		self.weights = np.array(vector)
 			
 	def setInputVector(self, vector):
 		self.expectedOutput = vector[0]
-		self.inputVector = [1]
-		for i in range(1,785):
-			self.inputVector.append(vector[i])
-	
+		self.inputVector[1:] = vector[1:]
+
 	def updateWeights(self,targetOutputMinusexceptedOutput):
-		for index in range(0,785):
-			self.weights[index] += learningRate*targetOutputMinusexceptedOutput*self.inputVector[index]
+		if targetOutputMinusexceptedOutput == 0:	#If (t-y) is zero no need to update weights just return
+			return		
+		self.weights += learningRate*targetOutputMinusexceptedOutput*self.inputVector
 	
 	def getWeights(self):
 		return self.weights
-		
+				
 	def getDotProduct(self):
-		return np.dot(np.array(self.weights), np.array(self.inputVector))
+		return np.dot(self.weights, self.inputVector)
 		
 	def getOutput(self):
-		#print "##########"
-		#print "Size of weights ",len(self.weights)
-		#print "Size of input vector ",len(self.inputVector)
-		
-		if np.dot(np.array(self.weights), np.array(self.inputVector)) > 0:
+		if np.dot(self.weights, self.inputVector) > 0:
 			return 1
 		else:
 			return 0
 #####################Perceptron Class ends#####################		
-		
-		
-"""
-def initiateRandomWeights():
-def calculateAccuracy():
-"""
-inputData2 = []
-def cleanData():
-	print "Cleaning data..."
-	for row in range(0,60000):
-		for col in range(0,785):
-			if col != 0:
-				inputData[row][col] = float(inputData[row][col])/255.0
-			else:
-				inputData[row][col] = int(inputData[row][col])
-	
-#def calculateAccuracy():
-	
+
+#Prints matrix 
 def printConfusionMatrix(matrix):
 	for i in range(0,10):
 		for j in range(0,10):
 			print matrix[i][j],
 		print "\n"
-		
-def initConfusionMatrix():
-	###Initialize confusionMatrix (All zeros)
-	for i in range(0,10):
-		new = []	
-		for j in range(0,10):
-			new.append(0)
-		confusionMatrix.append(new)
+
+"""		
+Arguments:
+inputVector: Input vector of shape (1, 785)
+We get weights of all perceptrons and make an array of size (785, 10)
+Compute the dot product so that we will get an array of shape (1,10)
+Then index of maximum dot product is returned
+"""
+def getIndexofMaxDotProduct(inputVector):
+	tempWeights = []
+	for i in range(0, 10):
+		new = []
+		new = perceptron[i].getWeights()
+		tempWeights.append(new)
+	tempWeights = np.array(tempWeights)
+	a = tempWeights.transpose()
+	b = np.dot(inputVector, a)
+	return np.argmax(b) 
 	
-def calculateAccuracy():
-	rightCount = 0;
-	for i in range (0,60000):
-		exceptedOutput = inputData[i][0]
-		maxDotProduct = 0
-		indexOfMaxDotProduct = 10
-		for j in range(0,10):
-			perceptron[j].setInputVector(inputData[i])
-			temp = perceptron[j].getDotProduct()
-			if maxDotProduct < temp:
-				maxDotProduct = temp
-				indexOfMaxDotProduct = j
-		if exceptedOutput == indexOfMaxDotProduct:
-			rightCount += 1
-	return (rightCount/60000.0)*100
-	"""
-	noOfDiagonalOnes = 0
-	totalNumberOfOnes = 0;
-	for i in range(0,10):
-		for j in range(0,10):
-			if confusionMatrix[i][j] == 1:
-				totalNumberOfOnes += 1
-				if i == j:
-					noOfDiagonalOnes += 1
-					
-	if totalNumberOfOnes == 0:
-		return 0
-	return float(noOfDiagonalOnes)/float(totalNumberOfOnes)	
-	"""
+"""
+Arguments:
+newInputData - passing inpudata as a parameter 
+newTestData - data from the testing dataset
+epochCycle - No of epoch cycle
 
-def processInput(tuple):
-	(pIndex, exceptedOutput) = tuple
-	perceptron[pIndex].setInputVector(inputData[trainingSet])
-	temp = perceptron[pIndex].getOutput()
-		
-	#update confusion matrix
-	#if temp == 1:
-	#	confusionMatrix[pIndex][exceptedOutput] = 1
-		
-	if ((temp == 1) and (pIndex != exceptedOutput)) or (temp == 0) and (pIndex == exceptedOutput):
-		if temp == 1:
-			perceptron[pIndex].updateWeights(-1)
-		else:
-			perceptron[pIndex].updateWeights(1)
+We iterate through the whole training and test data set.
+Calculate maximum dot product for each iteration.
+Enter that entry in confusion matrix
+Calculate accuracy using confusion matrix.
+Display confusion matrix of test set if it is the last epoch
+"""
+def calculateAccuracy(newInputData, newTestData, epochCycle):
+	a = np.zeros(785)
+	a[0] = 1.0
+	trainingConfusionMatrix = np.zeros((10,10))
+	testConfusionMatrix = np.zeros((10,10))
+	
+	for i in range(0,60000):
+		a[1:] = newInputData[i,1:]
+		index = getIndexofMaxDotProduct(a)
+		trainingConfusionMatrix[newInputData[i][0]][index] += 1
+	for i in range(0,10000):
+		a[1:] = newTestData[i,1:]
+		index = getIndexofMaxDotProduct(a)
+		testConfusionMatrix[newTestData[i][0]][index] += 1		
+	
+	testDiagonalSum = (np.sum(testConfusionMatrix.diagonal()))
+	testTotalSum = (np.sum(testConfusionMatrix))
+	trainingDiagonalSum = (np.sum(trainingConfusionMatrix.diagonal()))
+	trainTotalSum = (np.sum(trainingConfusionMatrix))
+	
+	testAccuracy.append(testDiagonalSum*100/testTotalSum)
+	trainingAccuracy.append(trainingDiagonalSum*100/trainTotalSum)
 
+	if epochCycle == 49:
+		printConfusionMatrix(testConfusionMatrix)
+	
+	return (testDiagonalSum + trainingDiagonalSum)*100/(testTotalSum + trainTotalSum)
 	
 def main():
-	cleanData()
-	print "Data cleaned"
-	#time.sleep(3)
+	print "Reading CSV"
+	"""
+	Pandas read csv
+	"""
+	#Reading training data
+	inputData = np.array(pandas.read_csv("mnist_train.csv", header = -1),np.float)
+	inputData[:,1:] /= 255.0	
+	print inputData.shape
 	
-	"""
-	Calculate accuracy
-	Detect the "wrong" perceptrons
-	update weights of those perceptrons
-	"""
+	#Reading test data
+	testData = np.array(pandas.read_csv("mnist_test.csv", header = -1),np.float)
+	testData[:, 1:] /= 255.0
 		
+	print "Data cleaned"
 	
 	for index in range(0,10):
 		perceptron.append(Perceptron())
-		weight = []
-		for i in range(0,785):
-			weight.append(random.uniform(-0.5,0.5))
-		perceptron[index].setWeights(weight)
 	
-	"""
-	print inputData[0]
-	perceptron[0].setInputVector(inputData[0])
-	print perceptron[0].getOutput()
-	"""
-	
-	initConfusionMatrix()
-	#print confusionMatrix
-	printConfusionMatrix(confusionMatrix)
-	#calculateAccuracy for epoch 0
-	
-	print "Initial accuracy: ",calculateAccuracy()," %"
-
-
-	
+	print "Initial accuracy: ",calculateAccuracy(inputData, testData,0)," %"
+	print "Learning rate: ", learningRate
+		
+	#Executing for 50 epochs...
 	for epoch in range(0,50):
-		np.random.shuffle(inputData)
-		initConfusionMatrix()
-		for trainingSet in range(0,1000):
+		np.random.shuffle(inputData)	#Shuffling the input data
+		for trainingSet in range(0,60000):	#For 60000 test sets
 			exceptedOutput = inputData[trainingSet][0]
-			#inputs = range(0,10)
-			#results = Parallel(n_jobs=num_cores)(delayed(processInput)(i,exceptedOutput) for i in inputs)		
-			#tuple = (i, exceptedOutput)
-			#results = Parallel(n_jobs=10, backend = "threading")(map(delayed(processInput),tuple))
 			
-			#print "Training set ", trainingSet
-			
-			for pIndex in range(0, 10):
-				perceptron[pIndex].setInputVector(inputData[trainingSet])
+			for pIndex in range(0, 10):		#for 10 perceptrons
+				perceptron[pIndex].setInputVector(inputData[trainingSet,:])
 				temp = perceptron[pIndex].getOutput()
-			
-				#update confusion matrix
-				#if temp == 1:
-			#	confusionMatrix[pIndex][exceptedOutput] = 1
-			
-				if ((temp == 1) and (pIndex != exceptedOutput)) or (temp == 0) and (pIndex == exceptedOutput):
-					if temp == 1:
-						perceptron[pIndex].updateWeights(-1)
-					else:
-						perceptron[pIndex].updateWeights(1)
-			
-		print "Accuracy after epoch: ",epoch," ",calculateAccuracy(), "%"
-		#print "Epoch ",epoch
-#		time.sleep(5)
-	print "Accuracy after epoch",epoch," ",calculateAccuracy()," %"
+							
+				target = -1	#Invalid case
+				if pIndex == exceptedOutput:
+					target = 1
+				else:
+					target = 0
+				
+				perceptron[pIndex].updateWeights(target-temp)	#passing (t-y) as a parameter to function
 
-main()
+		print "Accuracy after epoch: ",epoch," ",calculateAccuracy(inputData,testData,epoch), "%"
+		
+	message = "Learning rate = "
+	message1 = learningRate
+	
+	#Plotting grpah
+	plt.plot(testAccuracy)
+	plt.plot(trainingAccuracy)
+	plt.ylabel('Accuracy')
+	plt.xlabel('Epochs')
+	plt.title(message+str(message1))
+	plt.savefig("ResultExtra.png")
+	
+main()	
